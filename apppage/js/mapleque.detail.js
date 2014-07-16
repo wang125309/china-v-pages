43bc2e5d7cc4d3c21de34406d68a16cd6e7481ce
mapleque <mapleque@gmail.com>
2014-07-16 23:41:38 +0800

    bug fix

--- apppage/js/mapleque.detail.js
+++ apppage/js/mapleque.detail.js
@@ -1,8 +1,8 @@
 (function(){
 $(function(){
     var index=document.location.hash||'';
-    index=index.length>0?index.substr(1):0;
-    index+=1;
+    index=index.length>0?index.substr(1):1;
+    index-=1;
     mapleque.rotate({
       contbox:$('.section-list'),
       dotbox:$('.dot-bar'),
