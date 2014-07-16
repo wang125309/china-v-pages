85cf145048e2e6367b946c91b712fc4ffc20ab64
mapleque <mapleque@gmail.com>
2014-07-16 23:39:44 +0800

    debug

--- apppage/js/mapleque.detail.js
+++ apppage/js/mapleque.detail.js
@@ -2,6 +2,7 @@
 $(function(){
     var index=document.location.hash||'';
     index=index.length>0?index.substr(1):0;
+    index+=1;
     mapleque.rotate({
       contbox:$('.section-list'),
       dotbox:$('.dot-bar'),
