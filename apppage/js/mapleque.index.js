a5e521e991c624a24f27a1e440b71e454218f898
mapleque <mapleque@gmail.com>
2014-07-18 02:52:52 +0800

    revert

--- apppage/js/mapleque.index.js
+++ apppage/js/mapleque.index.js
@@ -9,11 +9,11 @@ function(){
   });
   $(document.body).bind('swipeLeft',function(e){
     if (e.target.className=='btn'||e.target.tagName=='IMG'||e.target.tagName=='A')return;
-    document.location.href="detail.html#6";
+    document.location.href="detail.html#0";
   });
   $(document.body).bind('swipeRight',function(e){
     if (e.target.className=='btn'||e.target.tagName=='IMG'||e.target.tagName=='A')return;
-    document.location.href="detail.html#0";
+    document.location.href="detail.html#6";
   });
 });
 })();
