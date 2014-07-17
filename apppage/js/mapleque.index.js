baf5c75e3fa656e41793feb8ac5f66bbc48069d6
mapleque <mapleque@gmail.com>
2014-07-18 01:36:44 +0800

    fix 5

--- apppage/js/mapleque.index.js
+++ apppage/js/mapleque.index.js
@@ -7,5 +7,13 @@ function(){
     leftarr:$('.arr-left a'),
     rightarr:$('.arr-right a')
   });
+  $(document.body).bind('swipeLeft',function(e){
+    if (e.target.className=='btn'||e.target.tagName=='IMG'||e.target.tagName=='A')return;
+    document.location.href="detail.html#6";
+  });
+  $(document.body).bind('swipeRight',function(e){
+    if (e.target.className=='btn'||e.target.tagName=='IMG'||e.target.tagName=='A')return;
+    document.location.href="detail.html#0";
+  });
 });
 })();
